9d93425d871f0c0fd47af378c9f795538863605f
Ilya <ilyavodop1@gmail.com>
2021-05-27 20:14:39 +0500

    Сreated a script player 2b and a bullet script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Offset;
+    public GameObject Bullet;
+    public Transform ShotPoint;
+
+    private float timeBtnShots;
+    public float startTimeBtnShots;
+
+    public static Player Instance;
+
+    void Awake()
+    {
+
+        Instance = this;
+    }
+
+    void OnDerstroy()
+    {
+        Instance = null;
+    }
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + Offset);
+        if (timeBtnShots <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(Bullet, ShotPoint.position, ShotPoint.transform.rotation);
+                timeBtnShots = startTimeBtnShots;
+            }
+        }
+        else
+        {
+            timeBtnShots -= Time.deltaTime;
+        }
+    }
+}
